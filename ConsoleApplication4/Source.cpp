763f7c8ff7afa8ce9e54a1c4828db47226b47e78
Евгений <@AYAKOVLEV>
2017-11-24 18:33:51 +0600

    Add project files.

---
+++ ConsoleApplication4/Source.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+#include <iostream>
+
+#define HIGNT 100L
+#define Width 32.5F
+#define hught 100uf
+#define HhCar 'G'
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+
+	long double  b = 56;
+
+//	cout << sizeof(b) << endl;
+
+
+
+	return 0;
+}
